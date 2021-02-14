0e6c1d24fd43502f3772673abf242fa7a43d4e60
Ethan D <54340089+TheFrostSpartan@users.noreply.github.com>
2021-02-13 20:17:20 -0700

    Added Health Bar
    Added health bar and rudimentary damage tracking

---
+++ Assets/Scripts/BossHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
+    public GameObject player;
+    public int range;
+    public int damageInflicted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Vector3.Distance(transform.position, player.transform.position) < range){
+            TakeDamage(damageInflicted);
+        }
+    }
+
+    void TakeDamage(int damage){
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
