57d9a1a648f7f5ba5c83546ecfc0bfadde58ab03
Matt Mader <mcmader1@gmail.com>
2021-02-26 16:09:07 -0700

    TRAD-58 TRAD-67 TRAD-69 TRAD-68 TRAD-76 Added Item Placement
    Added Transparent Objects
    Added Object Rotation
    Added Object Placement
    And more

---
+++ Assets/Scripts/BlueprintScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BlueprintScript : MonoBehaviour
+{
+    RaycastHit hit;
+    Vector3 createLocation;
+    public GameObject prefab;
+
+    void Start()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if(Physics.Raycast(ray, out hit, 50000.0f, (1 << 8)))
+        {
+            transform.position = hit.point;
+        }
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if(Physics.Raycast(ray, out hit, 50000.0f))
+        {   
+            Vector3 tempLocation = hit.point;
+            tempLocation.y += GetComponent<MeshFilter>().mesh.bounds.extents.y*transform.localScale.y;
+            transform.position = tempLocation;
+        }
+        
+        if(Mouse.current.scroll.ReadValue().y < 0)
+        {
+            transform.Rotate(Vector3.up * 10f, Space.Self);
+        }
+
+        if(Mouse.current.scroll.ReadValue().y > 0)
+        {
+            transform.Rotate(Vector3.down * 10f, Space.Self);
+        }
+
+        if (Mouse.current.leftButton.ReadValue() == 1)
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/spawnObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnObject : MonoBehaviour
+{
+    public GameObject wall;
+    public GameObject rock;
+
+    public void createWall()
+    {
+        Instantiate(wall);
+    }
+
+    public void createRock()
+    {
+        Instantiate(rock);
+    }
+}
