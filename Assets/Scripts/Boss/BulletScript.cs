5e75e041e1b3330e67aca3fac435135880d81ccf
Matt Mader <mcmader1@gmail.com>
2021-03-15 04:12:56 -0600

    TRAD-82 Bullet Prefab and Bullet Script
    Just shoots in the direction it is facing
    Has a few bugs and some issues with particles, still a WIP

---
+++ Assets/Scripts/Boss/BulletScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    [System.NonSerialized] public int damageDealt = 0;
+    [System.NonSerialized] public float movementSpeed = 50;
+
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * movementSpeed;
+        //gameObject.GetComponentInChildren<ParticleSystem>().gameObject.transform.Rotate(0, 0, 0);
+    }
+
+    void OnTriggerEnter(Collider other)    
+    {
+        if(other.gameObject.name == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealth>().damagePlayer(damageDealt);
+        }
+        if(other.gameObject.name != "Boss" && !other.gameObject.name.Contains("Bullet"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
