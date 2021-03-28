75a768e58bdd0c8e2b216d1c5611721ee31aab3c
Jakob <horvath.j17@gmail.com>
2021-03-27 22:20:14 -0700

    TRAD-120 / reoriented electricity vfx

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.VFX;
 
 public class PlayerAttack : MonoBehaviour
 {
     public ParticleSystem attackParticles;
+    public VisualEffect electricity;
 
     private float particleStoppingSpeed = 0.1f;
     //private float attackCooldown = 25f; // future use
@@ -24,23 +26,42 @@ public class PlayerAttack : MonoBehaviour
         attack = gameplayActionMap.FindAction("Attack");
         attack.performed += ctx =>
         {
-            attackParticles.Play();
-            currentAttackParticlesCooldown = 0;
+            //attackParticles.Play();
+            //currentAttackParticlesCooldown = 0;
+            electricity.Play();
         };
     }
 
     void FixedUpdate()
     {
         // particles are active
-        if (attackParticles.isEmitting)
-        {
-            currentAttackParticlesCooldown += particleStoppingSpeed; // this makes sense, trust me
+        //if (attackParticles.isEmitting)
+        //{
+        //    currentAttackParticlesCooldown += particleStoppingSpeed; // this makes sense, trust me
 
-            if (currentAttackParticlesCooldown > attackParticlesCooldown)
-            {
-                attackParticles.Stop();
-                currentAttackParticlesCooldown = 0;
-            }
+        //    if (currentAttackParticlesCooldown > attackParticlesCooldown)
+        //    {
+        //        attackParticles.Stop();
+        //        currentAttackParticlesCooldown = 0;
+        //    }
+        //}
+
+        if (electricity.isActiveAndEnabled)
+        {
+            Transform playerTransform = gameObject.transform;
+
+            electricity.gameObject.transform.position = transform.forward + new Vector3(playerTransform.position.x + 1.5f,
+                                                                                        playerTransform.position.y,
+                                                                                        playerTransform.position.z + 1.0f);
+
+            //electricity.gameObject.transform.rotation = new Quaternion(playerTransform.transform.rotation.x,
+            //                                                           playerTransform.transform.rotation.y,
+            //                                                           electricity.transform.rotation.z,
+            //                                                           electricity.transform.rotation.);
+
+            electricity.gameObject.transform.rotation = Quaternion.Euler(electricity.transform.rotation.x, 
+                                                                         electricity.transform.rotation.y, 
+                                                                         playerTransform.transform.rotation.z);
         }
     }
 }
