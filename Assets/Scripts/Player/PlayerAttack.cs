f2eeb1a12eba67d18bd6022a6ac3132276956ccd
Jakob <horvath.j17@gmail.com>
2021-03-28 22:09:36 -0700

    TRAD-120 / dual lightning, first bit of combo working

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -7,9 +7,13 @@ using UnityEngine.VFX;
 public class PlayerAttack : MonoBehaviour
 {
     public VisualEffect electricity;
+    public float comboTimeWindow = 1.0f;
+    public int maxCombo = 3;
+    public int comboCounter = 0;
+    //public float comboCooldown = 25f; // future use
 
-    //private float attackCooldown = 25f; // future use
     private float heightOffset = 0.5f;
+    private float handOffset = 0.5f;
 
     public InputActionAsset playerControls;
     private InputAction attack;
@@ -24,24 +28,94 @@ public class PlayerAttack : MonoBehaviour
         attack = gameplayActionMap.FindAction("Attack");
         attack.performed += ctx =>
         {
-            electricity.Play();
+            ComboAttack();
         };
     }
 
     void FixedUpdate()
     {
-        if (electricity.isActiveAndEnabled)
+
+    }
+
+    void ComboAttack()
+    {
+        comboCounter++;
+
+        switch (comboCounter)
         {
-            Transform playerTransform = gameObject.transform;
-
-            electricity.gameObject.transform.position = transform.forward + new Vector3(playerTransform.position.x,
-                                                                                        playerTransform.position.y + heightOffset,
-                                                                                        playerTransform.position.z);
-
-            electricity.gameObject.transform.rotation = new Quaternion(electricity.transform.rotation.x,
-                                                                       playerTransform.rotation.y,
-                                                                       electricity.transform.rotation.z,
-                                                                       playerTransform.rotation.w);
+            case 1:
+                PrepareLightning(true);
+                ShootRedLightning();
+                break;
+            case 2:
+                PrepareLightning(false);
+                ShootBlueLightning();
+                break;
+            //case 3:
+            //    ShootFireBall();
+            //    break;
+            //default:
+            //    break;
         }
+
+        if (comboCounter > 1)
+        {
+            comboCounter = 0;
+        }
+    }
+
+    void PrepareLightning(bool isRed)
+    {
+        Transform playerTransform = gameObject.transform;
+
+        float newHandOffset = isRed ? -handOffset : handOffset;
+
+        // set emission position (calculate offset for the right or left hand)
+        if (playerTransform.localEulerAngles.y <= 315.0f && playerTransform.localEulerAngles.y >= 225.0f)
+        {
+            electricity.gameObject.transform.position = transform.forward + new Vector3(playerTransform.position.x,
+                                                                                       playerTransform.position.y + heightOffset,
+                                                                                       playerTransform.position.z + newHandOffset);
+        }
+        else if (playerTransform.localEulerAngles.y >= 45.0f && playerTransform.localEulerAngles.y <= 135.0f)
+        {
+            electricity.gameObject.transform.position = transform.forward + new Vector3(playerTransform.position.x,
+                                                                                       playerTransform.position.y + heightOffset,
+                                                                                       playerTransform.position.z - newHandOffset);
+        }
+        else
+        {
+            electricity.gameObject.transform.position = transform.forward + new Vector3(playerTransform.position.x - newHandOffset,
+                                                                                       playerTransform.position.y + heightOffset,
+                                                                                       playerTransform.position.z);
+
+        }
+
+        // set emission rotation
+        electricity.gameObject.transform.rotation = new Quaternion(electricity.transform.rotation.x,
+                                                              playerTransform.rotation.y,
+                                                              electricity.transform.rotation.z,
+                                                              playerTransform.rotation.w);
+    }
+
+    void ShootBlueLightning()
+    {
+        electricity.SetVector4("BoltColor", new Vector4(0.356f, 0.772f, 0.984f, 1));
+        electricity.SetVector4("FlashColor", new Vector4(0.713f, 1.545f, 1.968f, 1));
+
+        electricity.Play();
+    }
+
+    void ShootRedLightning()
+    {
+        electricity.SetVector4("BoltColor", new Vector4(0.984f, 0.356f, 0.356f, 1));
+        electricity.SetVector4("FlashColor", new Vector4(1.968f, 0.713f, 0.713f, 1));
+
+        electricity.Play();
+    }
+
+    void ShootFireBall()
+    {
+
     }
 }
