69796e854ef69da06d5eebe4d8fcf892cf0dbd3a
Jakob <horvath.j17@gmail.com>
2021-04-07 14:31:29 -0700

    lil cleanup that won't brick the boss animator

--- Assets/Scripts/Player/Fireball.cs
+++ Assets/Scripts/Player/Fireball.cs
@@ -19,20 +19,4 @@ public class Fireball : MonoBehaviour
             manager.justHitBoss = true;
         }
     }
-
-    //int frameCount = 0;
-    //int maxCount = 15;
-
-    //void OnTriggerStay(Collider other)
-    //{
-    //    if (other.gameObject.tag == "Boss")
-    //    {
-    //        frameCount++;
-    //        if (frameCount > maxCount)
-    //        {
-    //            manager.justHitBoss = true;
-    //            frameCount = 0;
-    //        }
-    //    }
-    //}
 }
