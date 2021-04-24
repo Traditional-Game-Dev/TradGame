fd127f84e1728086031e8f698b39073787a758dd
Matt Mader <mcmader1@gmail.com>
2021-04-24 17:42:31 -0600

    Menu Audio Bug Fixed

---
+++ Assets/Audio/MusicFadeOut.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicFadeOut : MonoBehaviour
+{
+    public AudioMixer mixer;
+    void OnEnable()
+    {
+        StartCoroutine(FadeMixerGroup.StartFade(mixer, "MusicVolParam", 1, 0));
+    }
+}
