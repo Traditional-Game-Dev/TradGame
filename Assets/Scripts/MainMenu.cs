a775159a4f024e33daf67a4d261da889e2b2b9a6
Matt Mader <mcmader1@gmail.com>
2021-05-07 13:04:02 -0600

    Controller Support Main Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject initSelection;
+    private EventSystem eventSystem;
+
+
+    void Awake()
+    {
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+    }
+
+    void OnEnable()
+    {
+        if(Gamepad.current != null)
+        {
+            eventSystem.SetSelectedGameObject(initSelection);
+        }
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("levelOne");
