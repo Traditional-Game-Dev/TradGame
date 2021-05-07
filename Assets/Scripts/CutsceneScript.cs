77720cea4aaf5fdea59131a1b0a1f534b043bb8b
Jakob <horvath.j17@gmail.com>
2021-05-07 14:57:21 -0700

    cutscene is now disabling player and removing bombs

---
+++ Assets/Scripts/CutsceneScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneScript : MonoBehaviour
+{
+    private GameManager manager;
+
+    private void OnEnable()
+    {
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        manager.playerDisabled = true;
+        manager.playerIvin = true;
+
+        foreach (GameObject poisonBomb in GameObject.FindGameObjectsWithTag("Bomb"))
+        {
+            Destroy(poisonBomb);
+        }
+    }
+
+    private void OnDisable()
+    {
+        manager.playerDisabled = false;
+        manager.playerIvin = false;
+    }
+}
