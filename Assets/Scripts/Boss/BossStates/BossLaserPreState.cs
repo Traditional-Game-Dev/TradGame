33827cce6828d81301eb5ebc50c0a384af803efe
Ethan D <54340089+TheFrostSpartan@users.noreply.github.com>
2021-05-07 19:55:56 -0600

    Fixed Boss Warm Up Particle Effect

--- Assets/Scripts/Boss/BossStates/BossLaserPreState.cs
+++ Assets/Scripts/Boss/BossStates/BossLaserPreState.cs
@@ -8,12 +8,17 @@ public class BossLaserPreState : BossBaseState
     private ParticleSystem laserWarmUp;
     private float timerDuringAttacks;
     private float timeForWarmup;
+    public GameManager manager;
 
     public override void EnterState(BossController boss)
     {
         timerDuringAttacks = 0.0f;
         player = boss.player;
         laserWarmUp = boss.laserWarmUp;
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        if(manager.currLevel == 2){
+            laserWarmUp.transform.position = new Vector3(27, boss.transform.position.y + 5, 858);
+        }
         laserWarmUp.Play();
         laserWarmUp.GetComponentInParent<AudioSource>().Play();
         timeForWarmup = boss.timeForWarmup;
