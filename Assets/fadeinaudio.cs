430b53a0046c4413b4a8a6f9155d599500fcf1c4
Matt Mader <mcmader1@gmail.com>
2021-05-07 21:19:19 -0600

    More death cutscene and audio fixes

---
+++ Assets/fadeinaudio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class fadeinaudio : MonoBehaviour
+{
+    public AudioMixer mixer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FadeMixerGroup.StartFade(mixer, "MenuBGM", 2, 1));
+    }
+}
---
+++ Assets/fadeoutaudio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class fadeoutaudio : MonoBehaviour
+{
+    public AudioMixer mixer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FadeMixerGroup.StartFade(mixer, "GameBGM", 1, 0));
+    }
+}
